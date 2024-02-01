31901b80fbef45f079f3e82c99ff2710d519776e
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-01 15:11:34 +0300

    cors test

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
-
-copy
 import express from 'express';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
 // Настройки CORS
-app.use((req, res, next) => { 
+app.use(cors());
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.get("/", (req,res)=>{
-    res.send('работает');
-});
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send('работает');
+});
 app.post('/send-email', (req, res) => {
-    console.log(req.body);
-    
-    const { name, email, message } = req.body;
-  
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'testrocked@gmail.com',
-        pass: 'Qwer_1234'
-      }
-    });
-  
-    const mailOptions = {
-      from: 'testrocked@gmail.com',
-      to: 'bogdan_emdetei_petrov@mail.ru',
-      subject: 'New Message from React App',
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
-  
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-        res.status(500).send('Error');
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send('Success');
-      }
-    });
+  console.log(req.body);
+
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'testrocked@gmail.com',
+      pass: 'Qwer_1234'
+    }
+  });
+
+  const mailOptions = {
+    from: 'testrocked@gmail.com',
+    to: 'bogdan_emdetei_petrov@mail.ru',
+    subject: 'New Message from React App',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Success');
+    }
+  });
 });
 
 const PORT = 4444;
 app.listen(PORT, (err) => {
-    if (err) {
-        return console.log(err)
-    }
+  if (err) {
+    return console.log(err)
+  }
 
-    console.log("Server ok")
+  console.log("Server ok")
 });
