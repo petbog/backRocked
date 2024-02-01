5dd664dd34029a80c80511119e40555f1483c79c
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-01 14:50:43 +0300

    изменение cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
+import cors from 'cors';
 
 //импорт express
 const app = express()
@@ -7,11 +8,11 @@ app.use(express.json());
 app.use(cors());
 config()
 app.use((req, res, next) => { 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 //обращение к серверу
 
 
