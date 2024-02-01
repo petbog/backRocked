3aa8316f5509a8d63e7f3511c7df72e2f80df62d
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-01 14:37:23 +0300

    headers

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ import nodemailer from 'nodemailer';
 const app = express()
 app.use(express.json());
 app.use(cors());
-
+config()
+app.use((req, res, next) => { 
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 //обращение к серверу
 
 
