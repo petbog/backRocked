b93908830d3cc6972838c3b81c2ab87d90f11ecd
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-01 14:54:44 +0300

    изменение cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 config()
 app.use((req, res, next) => { 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
