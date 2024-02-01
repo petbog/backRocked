874910d18bbb03b4a296f8271ab6d2d292857991
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-01 22:10:48 +0300

    cors new

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ import nodemailer from 'nodemailer';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
