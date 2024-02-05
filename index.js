6c1c44a3be42b717b13b56110f08fca6e1cd4337
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-05 22:18:25 +0300

    corsv1

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-config()
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app/*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
