a410cda955c03b089eab973c8854edb9e981afdc
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-05 22:08:26 +0300

    отправка сообщений

--- index.js
+++ index.js
@@ -1,117 +1,53 @@
-// import express from 'express';
-// import nodemailer from 'nodemailer';
-// import cors from 'cors';
-
-// const app = express();
-
-// // Настройки CORS
-// app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
-
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send('работает');
-// });
-// app.post('/send-email', (req, res) => {
-//   console.log(req.body);
-
-//   const { name, email, message } = req.body;
-
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: 'testrocked@gmail.com',
-//       pass: 'Qwer_1234'
-//     }
-//   });
-
-//   const mailOptions = {
-//     from: 'testrocked@gmail.com',
-//     to: 'bogdan_emdetei_petrov@mail.ru',
-//     subject: 'New Message from React App',
-//     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-//   };
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(error);
-//       res.status(500).send('Error');
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//       res.send('Success');
-//     }
-//   });
-// });
-
-// const PORT = 4444;
-// app.listen(PORT, (err) => {
-//   if (err) {
-//     return console.log(err)
-//   }
-
-//   console.log("Server ok")
-// });
-import express from 'express';
 import nodemailer from 'nodemailer';
-import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://rocked.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(express.json());
+
+
+
 
 app.get("/", (req, res) => { res.send('работает'); });
 
 app.post('/send-email', (req, res) => {
-  console.log(req.body);
-  
+
   const { name, email, message } = req.body;
-  
+
+
   const transporter = nodemailer.createTransport({
-    host: 'imap.mail.ru',
-    port:993,
-    secure: false,
+    service: 'gmail',
     auth: {
-      user: 'testreckord@mail.ru',
-      pass: 'etrovtriper'
-    }
+      user: process.env.EMAIL,
+      pass: process.env.KEY
+    },
   });
-  
+
   const mailOptions = {
-    from: 'testreckord@mail.ru',
+    from: 'testrendermail@gmail.com',
     to: 'bogdan_emdetei_petrov@mail.ru',
-    subject: 'New Message from React App',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    subject: 'письмо отправленнное через hode.js',
+    text: `${name}, ${email}, ${message}`,
   };
-  
-  transporter.sendMail(mailOptions, function (error, info) {
+
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
-      res.status(500).send('Error');
+      console.error('Error sending email:', error);
+      res.status(400).json({
+        message: 'Ошибка при отправке письма'
+      });
     } else {
-      console.log('Email sent: ' + info.response);
-      res.send('Success');
+      console.log('Email sent successfully');
+      res.status(200).json({ message: 'Сообщение отправлено на почту' });
     }
   });
 });
 
-const PORT = 4444; app.listen(PORT, (err) => {
+const PORT = 4444;
+app.listen(PORT, (err) => {
   if (err) { return console.log(err) }
 
   console.log("Server ok")
 });
-// testreckord
-// etrovtriper
-// FzB2S4VExpBHekDBmthu
